3ec33bc35078d15841550820d66e97b95e37c4b2
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-04-19 23:59:50 +0530

    moving the input data page / class
    here moved the input data widget to another class and that class was imported to the main class

---
+++ lib/input_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: Theme(
+        data: ThemeData(
+            accentColor: Colors.deepPurpleAccent
+        ),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'input_data.dart';
 void main() => runApp(BMICalculator());
 
 Map<int, Color> color =
@@ -34,30 +34,3 @@ class BMICalculator extends StatelessWidget {
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(
-          accentColor: Colors.deepPurpleAccent
-        ),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
-      ),
-    );
-  }
-}
