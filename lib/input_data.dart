1f0d5010215a3bd703f92b6987f205ee4e239130
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-04-21 13:20:04 +0530

    Update input_data.dart

--- lib/input_data.dart
+++ lib/input_data.dart
@@ -13,65 +13,59 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(25.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 170.0,
-                    width: 200.0,
-                    color: Color(0xFF1D1F33),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Color(0xFF1D1F33),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Expanded(
-                  child: Container(
-                    height: 170.0,
-                    width: 200.0,
-                    color: Color(0xFF1D1F33),
+                  SizedBox(
+                    width: 10.0,
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Container(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                ],
+              ),
             ),
             SizedBox(
               height: 20.0,
             ),
-            Container(
-              height: 300.0,
-              width: double.infinity,
-              color: Color(0xFF1D1F33),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Color(0xFF1D1F33),
+              ),
             ),
             SizedBox(
               height: 20.0,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 170.0,
-                    width: 200.0,
-                    color: Color(0xFF1D1F33),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Color(0xFF1D1F33),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Expanded(
-                  child: Container(
-                    height: 170.0,
-                    width: 200.0,
-                    color: Color(0xFF1D1F33),
+                  SizedBox(
+                    width: 10.0,
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Container(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
