47b0dd05405058a69a17f92f871b663488ee9f45
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-04-22 11:52:52 +0530

    created the content card and made it dynamic

--- lib/input_data.dart
+++ lib/input_data.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const double bottomContainerHeight = 80;
 
 const roundedCardColor = Color(0xFF1D1F33);
 const bottomCardColor = Color(0xFFEB1555);
+const cardTextColor = Color(0xFF8D8E98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -26,11 +28,16 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: roundedCard(
                     colour: roundedCardColor,
+                    cardChild: cardContent(
+                      cardIcon: FontAwesomeIcons.mars,
+                      cardText: "Male",
+                    ),
                   ),
                 ),
                 Expanded(
                   child: roundedCard(
                     colour: roundedCardColor,
+                    cardChild: Container(),
                   ),
                 ),
               ],
@@ -39,6 +46,7 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: roundedCard(
               colour: roundedCardColor,
+              cardChild: Container(),
             ),
           ),
           Expanded(
@@ -47,11 +55,13 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: roundedCard(
                     colour: roundedCardColor,
+                    cardChild: Container(),
                   ),
                 ),
                 Expanded(
                   child: roundedCard(
                     colour: roundedCardColor,
+                    cardChild: Container(),
                   ),
                 ),
               ],
@@ -65,6 +75,33 @@ class _InputPageState extends State<InputPage> {
           )
         ],
       ),
+    );
+  }
+}
+
+class cardContent extends StatelessWidget {
+  cardContent({required this.cardIcon, required this.cardText});
+
+  final IconData cardIcon;
+  final String cardText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          cardIcon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 18.0,
+        ),
+        Text(
+          "$cardText",
+          style: TextStyle(fontSize: 18.0, color: cardTextColor),
+        )
+      ],
     );
   }
 }
